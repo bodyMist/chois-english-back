31a49ac25a513533555edfa254d004bf23ebc811
bodyMist <sky834459@gmail.com>
2022-03-25 01:45:38 +0900

    add checkAccount api

--- routers/memberController.js
+++ routers/memberController.js
@@ -7,12 +7,31 @@ const memberRouter = Router();
 memberRouter.post("/join", async (req, res) => {
   try {
     const member = new Member({ ...req.body });
-    console.log(member);
     await member.save();
     return res.send({ member });
   } catch (error) {
     console.log(error);
     return res.status(400).send({ error: error.message });
+  }
+});
+
+// 회원가입-계정 중복 확인
+memberRouter.get("/checkAccount", async (req, res) => {
+  const success = "success";
+  const failure = "failure";
+  try {
+    const { account } = req.query;
+    const member = await Member.findOne({ account });
+
+    if (member === null) {
+      console.log("아이디 사용가능");
+      res.status(200).json({ message: success });
+    } else {
+      res.status(200).json({ message: failure }); // 아이디 중복
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: error.message });
   }
 });
 
