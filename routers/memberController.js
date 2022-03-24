dc7c0d014dfc11a0409ee95c979d3b1c77d782bb
bodyMist <sky834459@gmail.com>
2022-03-25 02:44:50 +0900

    add mypage API & change api structure

--- routers/memberController.js
+++ routers/memberController.js
@@ -9,9 +9,12 @@ const failure = "failure";
 // 회원가입 post
 memberRouter.post("/join", async (req, res) => {
   try {
+    console.log("\nMember Join Request");
     const member = new Member({ ...req.body });
     await member.save();
-    return res.send({ member });
+
+    console.log(success);
+    return res.status(200).send({ member });
   } catch (error) {
     console.log(error);
     return res.status(400).send({ error: error.message });
@@ -21,15 +24,18 @@ memberRouter.post("/join", async (req, res) => {
 // 회원가입-계정 중복 확인
 memberRouter.get("/checkAccount", async (req, res) => {
   try {
+    console.log("\nMember CheckAccount Request");
     const { account } = req.query;
     const member = await Member.findOne({ account });
+    const resultMessage = null;
 
     if (member === null) {
       console.log("아이디 사용가능");
-      res.status(200).json({ message: success });
+      resultMessage = success;
     } else {
-      res.status(200).json({ message: failure }); // 아이디 중복
+      resultMessage = failure; // 아이디 중복
     }
+    return res.status(200).json({ message: resultMessage });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error: error.message });
@@ -39,11 +45,27 @@ memberRouter.get("/checkAccount", async (req, res) => {
 // 회원정보 조회
 memberRouter.get("/mypage", async (req, res) => {
   try {
-    console.log("\nmypage request");
+    console.log("\nMember Mypage Request");
     const { id } = req.query;
     const member = await Member.findById(id);
-    res.status(200).send({ member });
+
     console.log(success);
+    return res.status(200).send({ member });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error: error.message });
+  }
+});
+
+// 회원정보 수정-마이페이지 수정&비밀번호 수정 둘다 가능
+memberRouter.put("/update", async (req, res) => {
+  try {
+    console.log("\nMember Info Update Request");
+    const { id } = req.query;
+    await Member.updateOne({ _id: id }, { $set: { ...req.body } });
+
+    console.log(success);
+    return res.status(200).send({ success });
   } catch (error) {
     console.log(error);
     return res.status(500).send({ error: error.message });
