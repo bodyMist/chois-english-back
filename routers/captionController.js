e922c833c47c78eadca97068f58aa1b6c831d0b6
bodyMist <sky834459@gmail.com>
2022-03-25 20:24:38 +0900

    rename image api

--- routers/captionController.js
+++
@@ -1,6 +0,0 @@
-const { Router } = require("express");
-const { isValidObjectId } = require("mongoose");
-const captioningRouter = Router();
-
-const success = "success";
-const failure = "failure";
