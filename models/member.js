5095a985c5176897358e4a363e7995d12f317a98
bodyMist <sky834459@gmail.com>
2022-03-31 16:49:54 +0900

    modify member's account to unique key

--- models/member.js
+++ models/member.js
@@ -4,7 +4,7 @@ const { Schema, Types } = mongoose.Schema;
 const MemberSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    account: { type: String, required: true },
+    account: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
     images: [{ type: Types.ObjectId, ref: "Image" }],
