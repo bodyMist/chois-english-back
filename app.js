845738f9888128b9603df1d70862988b67b7bd9d
bodyMist <sky834459@gmail.com>
2022-04-07 23:07:46 +0900

    add API accessible server images

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const DB_URI = "mongodb://210.91.148.88:27017/chois-english";
 
 const memberController = require("./routers/memberController");
 const imageController = require("./routers/imageController");
+const staticController = require("./routers/staticController");
 
 const server = async () => {
   try {
@@ -16,10 +17,13 @@ const server = async () => {
       .then(() => console.log("Successfully connected to mongodb"))
       .catch((e) => console.log(e));
 
+    app.use("/static", express.static("static"));
     app.use(express.json());
     app.use(cors());
+
     app.use("/member", memberController);
     app.use("/image", imageController);
+    app.use("/static", staticController);
 
     app.listen(port, () => {
       console.log("start server on port3000");
---
+++ routers/staticController.js
@@ -0,0 +1,24 @@
+const { Router } = require("express");
+const fs = require("fs");
+const staticRouter = Router();
+
+const success = true;
+const failure = false;
+
+const imgUrl = "http://210.91.148.88:3000/static/";
+
+// 서버 이미지 접근하기
+staticRouter.get("/:imageName", async (req, res) => {
+  try {
+    console.log("\nServer Image Access");
+    result = imgUrl + req.params.imageName;
+    console.log(result);
+
+    return res.status(200).send(fs.readFileSync(result));
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ error: error.message, result: failure });
+  }
+});
+
+module.exports = staticRouter;
