3e9721fe062fe9654f969856de389a4068da8ec9
bodyMist <sky834459@gmail.com>
2022-04-07 23:08:18 +0900

    add a image save API

--- routers/imageController.js
+++ routers/imageController.js
@@ -1,10 +1,25 @@
 const { Router } = require("express");
 const { isValidObjectId } = require("mongoose");
 const { Image } = require("../models/image");
+const { Member } = require("../models/member");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    // set a localstorage destination
+    destination: (req, file, cb) => {
+      cb(null, "static/");
+    },
+    // convert a file name
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+});
 const imageRouter = Router();
 
 const success = "success";
 const failure = "failure";
+const imagesUrl = "";
 
 // 로컬 이미지 변환 요청
 imageRouter.post("/localCaption", async (req, res) => {
@@ -52,6 +67,25 @@ imageRouter.get("/random", async (req, res) => {
 
     console.log(success);
     return res.status(200).send({ result: success, image });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ error: error.message, result: failure });
+  }
+});
+
+// 이미지 저장(연동)
+imageRouter.post("/saveImage", upload.single("file"), async (req, res) => {
+  try {
+    console.log("\nSave Image Request");
+
+    // const member = await Member.findById(req.body.memberId);
+    const image = new Image({
+      imageName: req.file.originalname,
+      url: req.file.path,
+    });
+    console.log(image);
+    // res.json(req.file);
+    // console.log(req.file);
   } catch (error) {
     console.log(error);
     return res.status(400).send({ error: error.message, result: failure });
