f7bd47f9a12b9c2361b3f1711f04da764df84113
bodyMist <sky834459@gmail.com>
2022-04-10 02:33:24 +0900

    modify the local caption API and add incomplete API

--- routers/imageController.js
+++ routers/imageController.js
@@ -18,27 +18,48 @@ const upload = multer({
     },
   }),
 });
+const tempUpload = multer({
+  storage: multer.diskStorage({
+    // set a localstorage destination
+    destination: (req, file, cb) => {
+      cb(null, "temp/");
+    },
+    // convert a file name
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+});
 const success = "success";
 const failure = "failure";
 const imageUrl = "http://210.91.148.88:3000/static/";
+const tempImageUrl = "http://210.91.148.88:3000/temp/";
 
 // 로컬 이미지 변환 요청
-imageRouter.post("/localCaption", async (req, res) => {
-  try {
-    console.log("\nLocal Image Caption Request");
-    const imageBase64 = req.imageData;
+imageRouter.post(
+  "/localCaption",
+  tempUpload.single("example"),
+  async (req, res) => {
+    try {
+      console.log("\nLocal Image Caption Request");
+      const url = tempImageUrl + req.file.filename;
 
-    // construct Caption
+      // construct Caption
 
-    const caption = "sibal jonna himdleda";
+      // delete a temporarily saved image
+      const caption = "sibal jonna himdleda";
+      const blank = "sibal";
+      const dir = url.replace(tempImageUrl, "temp/");
+      fs.unlinkSync(dir);
 
-    console.log(success);
-    return res.status(200).send({ result: success, caption });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).send({ error: error.message, result: failure });
+      console.log(success);
+      return res.status(200).send({ result: success, caption, blank });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send({ error: error.message, result: failure });
+    }
   }
-});
+);
 
 // 서버 연동 이미지 변환 요청
 imageRouter.post("/serverCaption", async (req, res) => {
@@ -120,6 +141,25 @@ imageRouter.delete("/deleteImage", async (req, res) => {
 
     const dir = image.url.replace(imageUrl, "static/");
     fs.unlinkSync(dir);
+    console.log(success);
+    return res.status(200).send({ result: success });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ error: error.message, result: failure });
+  }
+});
+
+// 회원 이미지 조회
+imageRouter.get("/getMemberImages", async (req, res) => {
+  try {
+    console.log("\nGet Member's Image Request");
+    const imageIds = (
+      await Member.findById({ _id: req.query.id }).select("-_id images")
+    ).images;
+    console.log(imageIds);
+
+    //const images = await Image.find({})
+
     console.log(success);
     return res.status(200).send({ result: success });
   } catch (error) {
