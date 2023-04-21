6d3268db2b527f9620577e30c69e82988d1bfd8d
bodyMist <sky834459@gmail.com>
2023-04-22 01:41:43 +0900

    build: apply serverless version to deploy on lambda

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
-const app = express();
-const port = 3000;
+const awsServerlessExpress = require("aws-serverless-express");
 
 require("dotenv").config();
 const mongoose = require("mongoose");
@@ -11,26 +10,27 @@ const memberController = require("./routers/memberController");
 const imageController = require("./routers/imageController");
 const staticController = require("./routers/staticController");
 
-const server = async () => {
-  try {
-    await mongoose
-      .connect(process.env.DB_URI)
-      .then(() => console.log("Successfully connected to mongodb"))
-      .catch((e) => console.log(e));
-    mongoose.set("debug", true);
-    app.use("/static", express.static("static"));
-    app.use(express.json());
-    app.use(cors());
-    app.use(fileUpload());
+const app = express();
+app.use("/static", express.static("static"));
+app.use(express.json());
+app.use(cors());
+app.use(fileUpload());
 
-    app.use("/member", memberController);
-    app.use("/image", imageController);
-    app.use("/static", staticController);
-    app.listen(port, () => {
-      console.log("Start Server on port 3000");
+app.use("/member", memberController);
+app.use("/image", imageController);
+app.use("/static", staticController);
+
+const server = awsServerlessExpress.createServer(app);
+
+module.exports.index = (event, context) => {
+  mongoose
+    .connect(process.env.DB_URI)
+    .then(() => {
+      console.log("Successfully connected to mongodb");
+      awsServerlessExpress.proxy(server, event, context);
+    })
+    .catch((err) => {
+      console.log("Falied to connect to mongodb : ", err);
+      context.fail(err);
     });
-  } catch (error) {
-    console.log(error);
-  }
 };
-server();
